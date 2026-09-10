44e30e38a22095d6344c3269c984c69650c29188
soboj-paul <sobojpaul.me@gmail.com>
2026-09-11 01:54:03 +0600

    nav section done

---
+++ src/Components/Nav.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import logoText from "../assets/logo.png";
+
+const Nav = () => {
+  return (
+    <nav className="py-4 border-b border-gray-200">
+      <div className="container mx-auto px-12 grid grid-cols-4 gap-4 items-center">
+        <div className="col-span-1 flex justify-items-start gap-2">
+          <img src={logoText} alt="logo" />
+          <h2 className="text-2xl font-bold">Dev<span className="text-[#DB2777]">Stack</span></h2>
+        </div>
+        <div className="col-span-2">
+          <ul className="flex justify-center items-center gap-8">
+            <a className="text-[#DB2777]" href="">
+              <li>Home</li>
+            </a>
+            <a href="">
+              <li>Tachnologies</li>
+            </a>
+            <a href="">
+              <li>Project</li>
+            </a>
+            <a href="">
+              <li>About</li>
+            </a>
+            <a href="">
+              <li>Contact</li>
+            </a>
+          </ul>
+        </div>
+        <div className="col-span-1 flex justify-end gap-4">
+          <button className="cursor-pointer">Sign In</button>
+          <button className="bg-[#D91B7E] rounded-3xl text-[#ffff] py-2 px-4 cursor-pointer">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
